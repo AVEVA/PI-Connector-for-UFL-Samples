76c05a0d014ec45a0f6fad42d016ac3d9cc053c1
Jerome Lefebvre <jerome.p.lefebvre@gmail.com>
2016-01-01 07:07:48 +0900

    Switch to using a session

--- piuflput.py
+++ piuflput.py
@@ -18,46 +18,33 @@ The syntax is: python piufl.py REST-URL file
 
 Parameters:
     rest-ufl - The Address specified in the Data Source configuration of the Connector
-    file - Data file to be processed by the Connector 
-	
+    file - Data file to be processed by the Connector
+
 Example:
     python piuflput.py https://<server>:5460/connectordata/value value.csv
 
 	"""
 import argparse
 import requests
-import json
 import sys
 
-### Process arguments
+requests.packages.urllib3.disable_warnings()
+
+# Process arguments
 
 parser = argparse.ArgumentParser(description='POST file contents to PI Connector for UFL')
 parser.add_argument('resturl',
                         help='REST endpoint address')
-parser.add_argument('file', 
+parser.add_argument('file',
                    help='Data file to be POST-ed')
 
 args = parser.parse_args()
 
-
-### Read the file contents
-
-try:
-	fh = open(args.file,"r")
-	data = fh.read()
-	fh.close()
-	data = ''.join(data)
-except:
-	print("Unexpected error: ", sys.exc_info()[1])
-	sys.exit(1)
-
-### PUT the file to the REST endpoint
-
-try:
-	headers = {'content-type': 'text/html','Accept': 'text/plain'}
-	request = requests.put(args.resturl, data=data, verify=False)
-	print(request.text)
-	print(request.status_code)
-except:
-	print("Unexpected error: ", sys.exc_info()[1])
-	sys.exit(1)
+s = requests.session()
+s.auth = ('', '')
+# Read the file contents
+with open(args.file,"r") as f:
+    data = ''.join(f.readlines())
+    request = s.put(args.resturl, data=data, verify=False)
+    print(request.text)
+    print(request.status_code)
